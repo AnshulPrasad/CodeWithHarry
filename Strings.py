27a91a05e034fd889ad83707cc9ec0e43b1db6b5
Anshul Prasad <anshulprasad5440.com>
2025-12-07 21:59:26 +0530

    day 11

---
+++ Strings.py
@@ -0,0 +1,16 @@
+name1 = "anshul" # like an array of characters
+name2 = 'prasad'
+
+variable1 = 'He sad "I am good."'
+variable2 = """Helo
+1
+2
+3
+4
+"""
+
+print(variable2)
+
+# print(name1[3])
+for i in range(len(name1)):
+    print(name1[i])
