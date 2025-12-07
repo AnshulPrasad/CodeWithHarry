14468cab170fd26f133192dc5f008bb914ce89a2
Anshul Prasad <anshulprasad5440.com>
2025-12-07 21:05:31 +0530

    day 5

---
+++ Comments, Escape Sequences & Print Statement.py
@@ -0,0 +1,14 @@
+# multiline comment
+"""
+don't remove this line
+don't remove this line
+don't remove this line
+don't remove this line
+"""
+
+# single line comment
+
+print("I am\" hello \"you\nnull papaya") # excape sequence \n and \" are used
+
+print("hey",6,7,sep='%', end='999')
+print("anshul")
