9c4f3e59941194897c289300343eb6d9a40a17ee
Anshul Prasad <anshulprasad5440.com>
2025-12-07 21:38:04 +0530

    day 9

---
+++ Typecasting.py
@@ -0,0 +1,8 @@
+# int(), float(), str(), ord(), hex(), oct(), tuple(), list(), dict(), set()
+a= "1"
+b= "2"
+print(a+b)
+print(int(a)+int(b))# explicit typecasting
+
+c,d= 1,2.0
+print(c+d)# implicit typecasting
