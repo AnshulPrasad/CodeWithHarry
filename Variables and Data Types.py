2782c5f14f1d7b8de82bd49fe95430ed86e29b6e
Anshul Prasad <anshulprasad5440.com>
2025-12-07 21:21:04 +0530

    day 6

---
+++ Variables and Data Types.py
@@ -0,0 +1,30 @@
+a = 1
+print(a)
+
+b = "anshul"
+print(b)
+
+c= 1.1
+print(c)
+
+d = None
+print(d)
+
+e = complex(8,2)
+
+f = True
+
+g = [1,2,3]
+h = {1,2,3}
+i = {"a":1}
+
+print("type of a is: ",type(a))
+print("type of b is: ",type(b))
+print("type of c is: ",type(c))
+print("type of d is: ",type(d))
+print("type of e is: ",type(e))
+print("type of f is: ",type(f))
+print("type of g is: ",type(g))
+print("type of h is: ",type(h))
+print("type of i is: ",type(i))
+# everything in python is an object
