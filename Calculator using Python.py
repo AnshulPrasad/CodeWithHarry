f2417c81d1bcd7374f906e340a6a395bf9179ed0
Anshul Prasad <anshulprasad5440.com>
2025-12-09 14:03:25 +0530

    day 7

--- Calculator using Python.py
+++ Calculator using Python.py
@@ -1,10 +1,10 @@
 a = 15
 b = 4
 
-print(f"Addition of {a} and {b} is: {a+b}")
-print(f"Subtraction of {a} and {b} is: {a-b}")
-print(f"Multiplication of {a} and {b} is: {a*b}")
-print(f"Division of {a} and {b} is: {a/b}")
-print(f"Floor Division of {a} and {b} is: {a//b}")
-print(f"Modulo of {a} and {b} is: {a%b}")
-print(f"Power of {a} to {b} is: {a**b}")
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
+print(f"{a}// {b} = {a//b}")
+print(f"{a} % {b} = {a%b}")
+print(f"{a}** {b} = {a**b}")
