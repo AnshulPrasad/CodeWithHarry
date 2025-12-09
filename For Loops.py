aa92b8d8efcbb022183070b118112938aad13ec3
Anshul Prasad <anshulprasad5440.com>
2025-12-09 18:17:10 +0530

    day 17

---
+++ For Loops.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    print(i)
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    if color == "Red":
+        for i in color:
+            print(i)
