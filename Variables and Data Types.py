6ae8ae9a035a9f2aac89f45b2f3ea75b6ba67cd3
Anshul Prasad <anshulprasad5440.com>
2025-12-09 13:58:44 +0530

    day 6

--- Variables and Data Types.py
+++ Variables and Data Types.py
@@ -18,13 +18,17 @@ g = [1,2,3]
 h = {1,2,3}
 i = {"a":1}
 
-print("type of a is: ",type(a))
-print("type of b is: ",type(b))
-print("type of c is: ",type(c))
-print("type of d is: ",type(d))
-print("type of e is: ",type(e))
-print("type of f is: ",type(f))
-print("type of g is: ",type(g))
-print("type of h is: ",type(h))
-print("type of i is: ",type(i))
+def func1():
+    pass
+
+print("type of a is:",type(a))
+print("type of b is:",type(b))
+print("type of c is:",type(c))
+print("type of d is:",type(d))
+print("type of e is:",type(e))
+print("type of f is:",type(f))
+print("type of g is:",type(g))
+print("type of h is:",type(h))
+print("type of i is:",type(i))
+print("type of func1 is:",type(func1))
 # everything in python is an object
