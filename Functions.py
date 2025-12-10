b814c245e90bf737256925c1192831d749e0b4f9
Anshul Prasad <anshulprasad5440.com>
2025-12-10 09:08:48 +0530

    day 20

---
+++ Functions.py
@@ -0,0 +1,21 @@
+def calculateGmean(a,b):
+    gmean = (a*b)/(a+b)
+    print(gmean)
+
+def compare(a,b):
+    if a>b:
+        print("first number is greater")
+    elif a==b:
+        print("both numbers are equal")
+    else:
+        print("second number is greater")
+
+calculateGmean(8,7)
+calculateGmean(13,17)
+compare(8,8)
+compare(9,4)
+
+
+def func1():
+    pass
+compare(7,8)
