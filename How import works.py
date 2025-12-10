a8ea7a66fd808246e72ef337bbea631be6cd5bcf
Anshul Prasad <anshulprasad5440.com>
2025-12-10 23:07:04 +0530

    day 44

---
+++ How import works.py
@@ -0,0 +1,32 @@
+import math
+
+print(math.floor(4.2343))
+
+from math import sqrt, pi
+
+print(sqrt(4), pi)
+
+from math import *
+
+print(ceil(4.2343))
+print(factorial(4))
+
+from math import sqrt as s
+
+print(s(9))
+
+import math as m
+
+print(m.sqrt(16))
+
+import math
+
+print(dir(math))
+print(type(math.nan))
+
+from anshul import welcome, anshul
+import anshul
+
+welcome()
+print(anshul)
+print(dir(anshul))
