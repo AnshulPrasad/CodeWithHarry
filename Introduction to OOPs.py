e9a9e69d96d40fa4e05b7a70d60fb4c3f4508238
Anshul Prasad <anshulprasad5440.com>
2025-12-11 18:54:45 +0530

    day 56

---
+++ Introduction to OOPs.py
@@ -0,0 +1,5 @@
+# encapsulation, inheritance, polymorphism
+# oop map real world entities
+# it has properties and methods
+# object is an instance of a class
+# each object has its own properties but have common methods
