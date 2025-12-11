1942b04c166c5190e14f2e734098823c0d87ae4e
Anshul Prasad <anshulprasad5440.com>
2025-12-11 09:11:00 +0530

    day 45

--- anshul.py
+++ anshul.py
@@ -3,3 +3,9 @@ def welcome():
 
 
 anshul = "Hi"
+
+print(__name__)
+if (
+    __name__ == "__main__"
+):  # idom used to find if the scrip is run directly or imported as a module into another script.
+    welcome()
---
+++ if __name__ == "__main__".py
@@ -0,0 +1,3 @@
+import anshul
+
+anshul.welcome()
