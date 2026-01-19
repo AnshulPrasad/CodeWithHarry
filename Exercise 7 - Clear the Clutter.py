d7eddc4797abcf87696f8f2b9e57f43c8e2292fd
Anshul Prasad <anshulprasad5440.com>
2026-01-19 09:13:00 +0530

    upgrade

--- Exercise 7 - Clear the Clutter.py
+++ Exercise 7 - Clear the Clutter.py
@@ -1,14 +1,13 @@
 import os
 
 
-def rename(folder_path, format):
-    count = 1
-    for i in os.listdir(folder_path):
-        file_extension = i.split(".")[-1]
-        if file_extension == format:
-            file_path = os.path.join(folder_path, i)
-            os.rename(file_path, f"{folder_path}/{count}.{file_extension}")
-            count+=1
+def rename(folder_path, extension):
+    i = 1
+    for file in os.listdir(folder_path):
+        if file.endswith(extension):
+            file_path = os.path.join(folder_path, file)
+            os.rename(file_path, f"{folder_path}/{i}{extension}")
+            i += 1
 
 
-rename("/home/anshul/Documents/CodeWithHarry/temp", "txt")
+rename("/home/anshul/Documents/CodeWithHarry/temp", ".txt")
